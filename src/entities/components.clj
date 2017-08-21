30dfd457c311873c19afe4d123ce9dec93f61e40
Martin Madera <madera.martin@centrum.cz>
2017-08-21 21:18:35 +0200

    Moveable component has entity id

--- src/entities/components.clj
+++ src/entities/components.clj
@@ -2,35 +2,50 @@
   (:gen-class))
 
 (defn create-component
-  "Creates a component which an entity can be composed of. Name of the component
-  should be unique (is not validated in this function). State is an arbitrary value
-  to be used by component functions."
-  [name state]
-  {:name name :state state})
+  "Creates a component which an entity can be composed of. Name of the
+  component should be unique within the entity (is not validated in
+  this function, i.e. the entity should not have this component
+  already.). State is an arbitrary value to be used by component
+  functions. Entity-name is an arbitrary value which identifies the
+  entity."
+  [entity-name name state]
+  {:entity entity-name :name name :state state})
 
 (defn component?
   "Checks whether parameter is a component."
   [par]
   (and (map? par)
-       (not (nil? (:name par)))))
+       (not (nil? (:name par)))
+       (not (nil? (:entity par)))))
+
+(defn component-entity
+  "Retunrns an entity identifier which this component belongs to. The
+  entity identifier is the same value which was passed during its
+  creations."
+  [comp]
+  (:entity comp))
 
 (def moveable-name ::moveable)
 
 (defn moveable-create
-  "Creates a component which allows the entity to move on its own. Such entity
-  should not be a part of the map itself, however it knows its position itself.
-  If speed is not provided, 1.0 is assumed. Coords should be a vector and speed float.
-  If coords is not a vector and speed is not float, then map is returned
-  with :result false, :response nil and :reason string reason. If OK then map is returned
-  with :result true, :response component and :reason nil."
-  ([coords]
-   (moveable-create coords 1.0))
-  ([coords speed]
+  "Creates a component which allows the entity to move on its
+  own. Such entity should not be a part of the map itself, however it
+  knows its position itself. Entity-name is an arbitrary value which
+  identifies the entity. If speed is not provided, 1.0 is
+  assumed. Coords should be a vector and speed float. If coords is
+  not a vector and speed is not float, then map is returned
+  with :result false, :response nil and :reason string reason. If OK
+  then map is returned with :result true, :response component
+  and :reason nil."
+  ([entity-name coords]
+   (moveable-create entity-name coords 1.0))
+  ([entity-name coords speed]
    (if-not (vector? coords)
      {:result false :reason "Coordinates should be a vector."}
      (if-not (float? speed)
        {:result false :reason "Speed should be float."}
-       {:result true :response (create-component moveable-name {::coords coords ::speed speed})}))))
+       {:result true
+        :response (create-component entity-name moveable-name {::coords coords ::speed speed})}))))
 
 (defn moveable?
   "Checks whether the component is a Moveable."
--- test/entities/components_test.clj
+++ test/entities/components_test.clj
@@ -4,31 +4,40 @@
 
 (deftest create-component-test
   (testing "Creating a new empty component."
-    (let [ec (create-component "empty-name" "empty-state")]
-      (is (= "empty-name" (:name ec)) "Component created with correct name.")
-      (is (= "empty-state" (:state ec))) "Component created with correct state.")))
+    (let [ec (create-component "entity-name" "empty-name" "empty-state")]
+      (is (= "entity-name" (:entity ec)) "Component should be created with correct entity name.")
+      (is (= "empty-name" (:name ec)) "Component should be created with correct name.")
+      (is (= "empty-state" (:state ec))) "Component should be created with correct state.")))
 
 (deftest component?-test
-  (testing "Map with key :name is a component."
-    (is (true? (component? {:name "abc"}))))
-  (testing "If parameter is not map with :name key, it's not component."
+  (testing "Map with keys :entity and :name is a component."
+    (is (true? (component? {:entity "ent" :name "abc"}))))
+  (testing "If parameter is not map with :entity and :name keys, it's not component."
     (is (false? (component? "a string")) "A string should not be a component.")
-    (is (false? (component? {})) "A map without :name key should not be a component.")))
+    (is (false? (component? {})) "A map without :name or :entity key should not be a component.")
+    (is (false? (component? {:entity "a"})) "A map without :name key should not be a component.")
+    (is (false? (component? {:name "a"})) "A map without :entity key should not be a component.")))
+
+(deftest component-entity-test
+  (testing "Should return correct entity identifier."
+    (is (= "IDENT" (component-entity (create-component "IDENT" "c-n" {}))))))
 
 (deftest moveable-create-test
   (testing "Creating a new moveable component."
-    (let [mc (:response (moveable-create [5 6]))
-          [x y] (:entities.components/coords (:state mc))]
+    (let [mc (:response (moveable-create "ENT" [5 6]))
+          [x y] (:entities.components/coords (:state mc))
+          ent-id (component-entity mc)]
       (is (and (= 5 x) (= 6 y)) "Moveable component created with correct coordinates.")
-      (is (= moveable-name (:name mc)) "Moveable component created with correct name.")))
+      (is (= moveable-name (:name mc)) "Moveable component created with correct name.")
+      (is (= "ENT" ent-id) "Moveable component associated with correct entity identifier.")))
   (testing "Setting speed to a moveable component."
-    (let [mc1 (:response (moveable-create []))
-          mc2 (:response (moveable-create [] 5.9))]
+    (let [mc1 (:response (moveable-create "ENT" []))
+          mc2 (:response (moveable-create "ENT" [] 5.9))]
       (is (= 1.0 (:entities.components/speed (:state mc1))) "If created without setting speed, 1.0 should be assumed.")
       (is (= 5.9 (:entities.components/speed (:state mc2))) "If created with 5.9 speed, the speed should be 5.9.")))
   (testing "If moveable created with wrong data types, false :result should be returned."
-    (let [mr1 (:result (moveable-create nil 1.5))
-          mr2 (:result (moveable-create [] "a"))]
+    (let [mr1 (:result (moveable-create "ENT" nil 1.5))
+          mr2 (:result (moveable-create "ENT" [] "a"))]
       (is (false? mr1) "Creating moveable with non-vector coords should result in falsey :result.")
       (is (false? mr2) "Creating moveable with non-float speed should result in falsey :result."))))
 
@@ -36,21 +45,23 @@
   (testing "Moveable should be a component."
     (is (false? (moveable? {})) "An empty map is not component and should not be moveable."))
   (testing "Moveable component should have :name identical to moveable-name."
-    (is (true? (moveable? {:name moveable-name})))))
+    (is (true? (moveable? {:entity "ENT" :name moveable-name})))))
 
 (deftest moveable-coords-test
   (testing "Getting correct coordinates from moveable component."
-    (let [mc (moveable-create [1 2 3])]
+    (let [mc (:response (moveable-create "ENT" [1 2 3]))]
       (is (= [1 2 3] (moveable-coords mc)) "Got wrong coordinates from moveable component."))))
 
 (deftest moveable-speed-test
   (testing "Getting correct speed from moveable component."
-    (let [mc (moveable-create nil 5.5)]
+    (let [mc (:response (moveable-create "ENT" [] 5.5))]
       (is (= 5.5 (moveable-speed mc))))))
 
 (deftest moveable-move-to-test
   (testing "New coordinates should be assigned to moveable."
-    (let [mc (moveable-create [1 2 3])
+    (let [mc (:response (moveable-create "ENT" [1 2 3]))
           mm (moveable-move-to mc [4 5 6])
           new-coords (moveable-coords mm)]
       (is (= [4 5 6] new-coords) "Moveable should have been moved to correct "))))
+
+
