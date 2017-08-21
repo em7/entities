0e8fe6dff9471737a37ab72119f69b26e4212772
Martin Madera <madera.martin@centrum.cz>
2017-08-21 20:20:21 +0200

    Entities no longer hold references to its components.

--- src/entities/core.clj
+++ src/entities/core.clj
@@ -4,9 +4,7 @@
   (:require [clojure.string :as str]
             [entities.components :as cmp]))
 
-(def all-entities
-  "All entities which are in the system."
-  (ref '()))
+
 
 (defn entity-name-valid?
   "Checks whether an entity name is valid.
@@ -26,7 +24,7 @@
 (defn create-entity
   "Creates a named entity. The name should be a unique string.
   If entity creates successfully, returns a map with :result true 
-  :response {:entity newly created entity :entities all entities with the new one conjoined;
+  :response newly created entity
   otherwise :result false :response nil :reason string - the reason why it failed."
   [entities name]
   (let [name-valid (entity-name-valid? entities name)]
@@ -34,23 +32,13 @@
       {:result false :reason (:reason name-valid)}
       (let [entity {:name name :components '()}
             ents (conj entities entity)]
-        {:result true :response {:entity entity :entities ents}}))))
+        {:result true :response entity}))))
 
 
 
 ;; is adding a component to an entity really correct?
 
-(defn add-component
-  "Adds a component to the entity. Returns a new entity with added component."
-  [component entity]
-  (update entity :components #(conj % component)))
 
-(defn has-component?
-  "Checks whether the entity has a particular component.
-  comp-test is a function which takes one argument, a component, and
-  returns true when the component is the required one, false otherwise."
-  [comp-test entity]
-  (some comp-test (:components entity)))
 
 ;; when replacing component, use hashmap as smap
 ;; e.g.  (replace {mc mc2} (:components ent))
